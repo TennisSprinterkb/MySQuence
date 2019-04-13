ac9c81125100b967e55fe44fd044ae9086bd4774
Greeley Wentz <jasonhorowitz@Jasons-MacBook-Pro.local>
2019-04-13 15:33:03 -0400

    passport

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,59 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Home from "./components/Home";
+import axios from 'axios'
+import { Route, Link } from 'react-router-dom'
+// components
+
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      loggedIn: false,
+      username: null
+    }
+
+    this.getUser = this.getUser.bind(this)
+    this.componentDidMount = this.componentDidMount.bind(this)
+    this.updateUser = this.updateUser.bind(this)
+  }
+
+  componentDidMount() {
+    this.getUser()
+  }
+
+  updateUser (userObject) {
+    this.setState(userObject)
+  }
+
+  getUser() {
+    axios.get('/user/').then(response => {
+      console.log('Get user response: ')
+      console.log(response.data)
+      if (response.data.user) {
+        console.log('Get User: There is a user saved in the server session: ')
+
+        this.setState({
+          loggedIn: true,
+          username: response.data.user.username
+        })
+      } else {
+        console.log('Get user: no user');
+        this.setState({
+          loggedIn: false,
+          username: null
+        })
+      }
+    })
+  }
+
+
   render() {
     return (
       <div>
         <Home />
-        
+         
       
       </div>
     );
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
 // import { createRequireFromPath } from 'module';
 // import yoga from "../../components/images/yogaHome.jpg"
 // import { url } from 'inspector';
-import LoginInput from "../Login"
+// import LoginInput from "../Login"
+import LoginForm from "../LoginForm";
+import Signup from "../Signup"
+
 
 class Home extends React.Component {
     render() {
         return (
             
             <div>
-                <LoginInput />
+
+        <Route
+          path="/login"
+          render={() =>
+            <LoginForm
+              updateUser={this.updateUser}
+            />}
+        />
+        <Route
+          path="/signup"
+          render={() =>
+            <Signup/>}
+        />  
             </div>
         )
     }
