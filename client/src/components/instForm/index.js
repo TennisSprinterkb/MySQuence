398f71bab6b2febd005e04422e57acac2faf8110
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-04-19 00:19:47 -0400

    successful post route from login

--- client/src/components/instForm/index.js
+++ client/src/components/instForm/index.js
@@ -1,5 +1,5 @@
-import React, {Component} from './node_modules/react';
-import axios from './node_modules/axios';
+import React, {Component} from 'react';
+import axios from 'axios';
 
 class InstForm extends Component {
 
@@ -26,17 +26,23 @@ handleChange(event) {
 handleSubmit(event) {
     event.preventDefault()
     fetch("/getData")
-    .then((response) => response.json())
+    //.then((response) => {response.json()})
     .then((res) => {console.log(res) });
     console.log(this.state.username)
     // get data
-  fetch("/postData", {
+  fetch("/api/login", {
     method:'POST',
     headers: {
       'Accept':'application/json',
       'Content-Type':'application/json'
     },
-    body: JSON.stringify({username:this.state.username,password: this.state.password})
+    body: JSON.stringify({
+        userName:this.state.username,
+        passwordHash: this.state.password,
+    isTeacher: true,
+    certificationNumber: this.state.certificate,
+    emailAddress: this.state.email
+})
   })
   .then((response) => response.json())
   .then((res) => {console.log(res) });
--- server.js
+++ server.js
@@ -2,42 +2,44 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const md5 = require('md5');
-const db = require("./models")
-const user = require("./routes/user-api-routes")
-const sequence = require("./routes/sequence-api-routes")
+const db = require("./models");
+const routes = require("./routes");
+// const user = require("./routes/api/user-api-routes")
+// const sequence = require("./routes/api/sequence-api-routes")
 // Route requires
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json())
+
 app.get("/getData", (req, res) => {//must have the /routeName to work with React front end.
     // console.log("hi");
     res.json({testData:'testData'})
 });
 
-// app.post("/postData", (req, res) => {//must have the /routeName to work with React front end.
-//     console.log("Data from front end", req.body);
-// 	let username = req.body.username;
-// 	let password = md5(req.body.password);
-// 	console.log(password)
-// 	res.json({responseData:'responseTestData'})
-// });
+app.post("/postData", (req, res) => {//must have the /routeName to work with React front end.
+    console.log("Data from front end", req.body);
+	let username = req.body.username;
+	let password = md5(req.body.password);
+	console.log(password)
+	res.json({responseData:'responseTestData'})
+});
 
-// app.post("/postLogin", (req, res) => {//must have the /routeName to work with React front end.
-// 	console.log('Works!')
-//     console.log("Data from front end", req.body);
-// 	let username = req.body.username;
-// 	let password = md5(req.body.password);
-// 	console.log(password)
-// 	res.json({responseData:'responseTestData'})
-// });
-app.use(bodyParser.urlencoded({ extended: true }));
+app.post("/postLogin", (req, res) => {//must have the /routeName to work with React front end.
+	console.log('Works!')
+    console.log("Data from front end", req.body);
+	let username = req.body.username;
+	let password = md5(req.body.password);
+	console.log(password)
+	res.json({responseData:'responseTestData'})
+});
+//app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
-app.use(user);
-app.use(sequence);
+//app.use(user);
+app.use(routes);
 
 // listen on port 3000
 var PORT = process.env.PORT || 3001;
