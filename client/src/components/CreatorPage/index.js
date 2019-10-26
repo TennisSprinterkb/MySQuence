cb2b41aa369a8ab08c51b49f8be769d3e281b9d0
Ize Imai <izeimai@gmail.com>
2019-10-26 08:22:32 -0400

    Added the profile photos locally for creator page

--- client/src/components/CreatorPage/index.js
+++ client/src/components/CreatorPage/index.js
@@ -19,37 +19,37 @@ class CreatorPage extends React.Component {
         </div>
         <Row>
           <Col l={2} m={4} s={6}>
-            <Card key="1" header={<CardTitle image="client/src/assets/images/john.jpg" />}
+            <Card key="1" header={<CardTitle image={require("../../assets/images/ebeaudet.png")} />}
               title="Ed Beaudet">
               <a href="https://www.linkedin.com/in/edward-beaudet-78027433/">LinkedIn</a>
             </Card>
           </Col>
           <Col l={2} m={4} s={6}>
-            <Card key="2" header={<CardTitle image="" />}
+            <Card key="2" header={<CardTitle image={require("../../assets/images/iimai.jpg")} />}
               title="Ize Imai">
               <a href="https://www.linkedin.com/in/ize-imai-45757b29/">LinkedIn</a>
             </Card>
           </Col>
           <Col l={2} m={4} s={6}>
-            <Card key="3" header={<CardTitle image="" />}
+            <Card key="3" header={<CardTitle image={require("../../assets/images/jhorowitz.png")} />}
               title="Jason Horowitz">
               <a href="https://www.linkedin.com/in/jason-horowitz-0a362b5a/">LinkedIn</a>
             </Card>
           </Col>
           <Col l={2} m={4} s={6}>
-            <Card key="4" header={<CardTitle image="" />}
+            <Card key="4" header={<CardTitle image={require("../../assets/images/jpirrone.png")} />}
               title="Jennifer Pirrone">
               <a href="https://www.linkedin.com/in/jennifer-pirrone-59211519/">LinkedIn</a>
             </Card>
           </Col>
           <Col l={2} m={4} s={6}>
-            <Card key="5" header={<CardTitle image="" />}
+            <Card key="5" header={<CardTitle image={require("../../assets/images/kbray.jpeg")} />}
               title="Kevin Bray">
               <a href="https://www.linkedin.com/in/kevin-bray-53a194171/">LinkedIn</a>
             </Card>
           </Col>
           <Col l={2} m={4} s={6}>
-            <Card key="6" header={<CardTitle image="" />}
+            <Card key="6" header={<CardTitle image={require("../../assets/images/lsimmons.png")} />}
               title="Lily Simmons">
               <a href="https://www.linkedin.com/in/lily-simmons-311778121/">LinkedIn</a>
             </Card>
